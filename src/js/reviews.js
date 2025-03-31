91070fa611c0233424e2876d5ba0a2a09b2ededd
your_nameSandra <your_emailsandram0608@gmail.com>
2025-03-31 20:15:59 +0300

    add fix rewiews

---
+++ src/js/reviews.js
@@ -0,0 +1,71 @@
+import Swiper from 'swiper';
+import 'swiper/css';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const swiper = new Swiper('.reviews-swipper', {
+    // ЗБЕРІГАЄМО у ЗМІННУ
+    slidesPerView: 4,
+    spaceBetween: 16,
+    loop: false,
+    initialSlide: 0,
+    centeredSlides: false,
+    // navigation: {
+    //   nextEl: '.swiper-button-next',
+    //   prevEl: '.swiper-button-prev',
+    // },
+    keyboard: {
+      enabled: true,
+      onlyInViewport: true,
+    },
+    mousewheel: true,
+  });
+
+  swiper.on('slideChange', function () {
+    console.log('slide changed', swiper.activeIndex);
+    console.log(swiper);
+
+    if (swiper.activeIndex === 0) {
+      const divElement = document.querySelector('.reviews-button-left');
+
+      // Добавляем класс
+      divElement.classList.add('block');
+    }
+
+    if (swiper.activeIndex === 2) {
+      const divElement = document.querySelector('.reviews-button-right');
+
+      // Добавляем класс
+      divElement.classList.add('block');
+    }
+
+    if (swiper.activeIndex === 2 || swiper.activeIndex === 1) {
+      const divElement = document.querySelector('.reviews-button-left');
+
+      // Добавляем класс
+      divElement.classList.remove('block');
+    }
+
+    if (swiper.activeIndex === 0 || swiper.activeIndex === 1) {
+      const divElement = document.querySelector('.reviews-button-right');
+
+      // Добавляем класс
+      divElement.classList.remove('block');
+    }
+  });
+
+  document
+    .querySelector('.reviews-button-right')
+    .addEventListener('click', () => {
+      console.log('Клік по кнопці!');
+      console.log(swiper.params.navigation); // Тепер змінна swiper визначена
+      swiper.slideNext(); // Додаємо примусовий перехід до наступного слайда
+    });
+
+  document
+    .querySelector('.reviews-button-left')
+    .addEventListener('click', () => {
+      console.log('Клік по кнопці!');
+      console.log(swiper.params.navigation); // Тепер змінна swiper визначена
+      swiper.slidePrev(); // Додаємо примусовий перехід до наступного слайда
+    });
+});
