c3ab6b08839f024d86ea1ba537195dbe47faeff4
your_nameSandra <your_emailsandram0608@gmail.com>
2025-05-29 14:14:57 +0300

    fix new conteiner

--- src/js/header.js
+++ src/js/header.js
@@ -14,56 +14,26 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 });
 
-// document.addEventListener('DOMContentLoaded', () => {
-//   const headerOpenMenuBtn = document.querySelector("[data-menu-open='header']");
-//   const headerCloseMenuBtn = document.querySelector(
-//     "[data-menu-close='header']"
-//   );
-//   const headerMenuOverlay = document.querySelector("[data-menu='header']");
-//   const headerMenuLinks = document.querySelectorAll(
-//     '.js-header-mobile-menu-link'
-//   );
+// Modal_Mobile
+const openBtn = document.querySelector('[data-modal-mob-open]');
+const closeBtn = document.querySelector('[data-modal-mob-close]');
+const btnMobileMenu = document.querySelector('.header-mobile-menu-overlay');
+const btnCloseMenu = document.querySelector('.header-close-menu-btn');
 
-//   headerOpenMenuBtn.addEventListener('click', () => {
-//     headerMenuOverlay.classList.add('is-open');
-//   });
+// Відкриття модального вікна
+const openModal = () => {
+  btnMobileMenu.classList.add('is-open');
+  console.log('ggggg');
+  document.body.style.overflow = 'hidden';
+  // document.addEventListener('keydown', handleEscapeKey);
+};
+openBtn.addEventListener('click', openModal);
 
-//   headerCloseMenuBtn.addEventListener('click', () => {
-//     headerMenuOverlay.classList.remove('is-open');
-//   });
-
-//   headerMenuLinks.forEach(link => {
-//     link.addEventListener('click', () => {
-//       headerMenuOverlay.classList.remove('is-open');
-//     });
-//   });
-// });
-
-// document.addEventListener('DOMContentLoaded', function () {
-//   const headerMenuBtn = document.querySelector('.js-header-menu-btn');
-//   const headerDropdownMenu = document.querySelector('.js-header-dropdown-menu');
-
-//   function toggleHeaderMenu() {
-//     headerDropdownMenu.classList.toggle('open');
-//   }
-
-//   headerMenuBtn.addEventListener('click', function (event) {
-//     event.stopPropagation();
-//     toggleHeaderMenu();
-//   });
-
-//   document.addEventListener('click', function (event) {
-//     if (
-//       !headerMenuBtn.contains(event.target) &&
-//       !headerDropdownMenu.contains(event.target)
-//     ) {
-//       headerDropdownMenu.classList.remove('open');
-//     }
-//   });
-
-//   headerDropdownMenu.querySelectorAll('a').forEach(link => {
-//     link.addEventListener('click', function () {
-//       headerDropdownMenu.classList.remove('open');
-//     });
-//   });
-// });
+// Закриття модального вікна
+const closeModal = () => {
+  btnMobileMenu.classList.remove('is-open');
+  console.log('hhhhh');
+  document.body.style.overflow = 'hidden';
+  // document.addEventListener('keydown', handleEscapeKey);
+};
+btnCloseMenu.addEventListener('click', closeModal);
