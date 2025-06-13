7be9a6918ee601dbed511c6091e33abf45805964
your_nameSandra <your_emailsandram0608@gmail.com>
2025-06-13 19:25:10 +0300

    fix accordion

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,12 +1,16 @@
 import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
 
+const screenWidth = window.innerWidth;
+const isMobileOrTablet = screenWidth < 1440;
+
 document.addEventListener('DOMContentLoaded', function () {
   new Accordion('.faq-list-left.accordion', {
     duration: 300, // Швидкість анімації (в мс)
     showFirst: false, // Відкрити перший елемент за замовчуванням
     collapse: true, // Автоматично закривати інші елементи
     // openOnInit: [0],
+    showMultiple: isMobileOrTablet,
   });
 });
 
@@ -16,6 +20,7 @@ document.addEventListener('DOMContentLoaded', function () {
     showFirst: false, // Відкрити перший елемент за замовчуванням
     collapse: true, // Автоматично закривати інші елементи
     // openOnInit: [0],
+    showMultiple: isMobileOrTablet,
   });
 });
 
